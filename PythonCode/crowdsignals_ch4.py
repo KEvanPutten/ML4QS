cacd74529ebddbdf3a2a7f94dc3b608065c68ae0
KEvanPutten <ke.vanputten@gmail.com>
2018-06-10 23:37:14 +0200

    Cleanup of ch4 code

--- PythonCode/crowdsignals_ch4.py
+++ PythonCode/crowdsignals_ch4.py
@@ -21,7 +21,7 @@ DataViz = VisualizeDataset()
 # Read the result from the previous chapter, and make sure the index is of the type datetime.
 dataset_path = './intermediate_datafiles/'
 try:
-    dataset = pd.read_csv(dataset_path + 'test.csv', index_col=0)
+    dataset = pd.read_csv(dataset_path + 'chapter3_result_final.csv', index_col=0)
 except IOError as e:
     print('File not found, try to run previous crowdsignals scripts first!')
     raise e
@@ -35,11 +35,12 @@ milliseconds_per_instance = (dataset.index[1] - dataset.index[0]).microseconds/1
 # Chapter 4: Identifying aggregate attributes.
 
 # First we focus on the time domain.
+NumAbs = NumericalAbstraction()
 
 # Set the window sizes to the number of instances representing 5 seconds, 30 seconds and 5 minutes
+# This part is for generating plots
 window_sizes = [int(float(5000)/milliseconds_per_instance), int(float(0.5*60000)/milliseconds_per_instance), int(float(5*60000)/milliseconds_per_instance)]
 
-NumAbs = NumericalAbstraction()
 dataset_copy = copy.deepcopy(dataset)
 for ws in window_sizes:
     dataset_copy = NumAbs.abstract_numerical(dataset_copy, ['acc_phone_x'], ws, 'mean')
@@ -49,17 +50,16 @@ for ws in window_sizes:
 
 DataViz.plot_dataset(dataset_copy, ['acc_phone_x', 'acc_phone_x_temp_mean', 'acc_phone_x_temp_std', 'acc_phone_x_temp_MAD', 'acc_phone_x_temp_kurtosis', 'label'], ['exact', 'like', 'like', 'like', 'like', 'like'], ['line', 'line', 'line', 'line', 'line', 'points'])
 
+# Compute the time domain metric and add them to the dataser
 ws = int(float(0.5*60000)/milliseconds_per_instance)
 selected_predictor_cols = [c for c in dataset.columns if not 'label' in c]
 dataset = NumAbs.abstract_numerical(dataset, selected_predictor_cols, ws, 'mean')
 dataset = NumAbs.abstract_numerical(dataset, selected_predictor_cols, ws, 'std')
 
-
 CatAbs = CategoricalAbstraction()
 dataset = CatAbs.abstract_categorical(dataset, ['label'], ['like'], 0.03, int(float(5*60000)/milliseconds_per_instance), 2)
 
 # Now we move to the frequency domain, with the same window size.
-
 FreqAbs = FourierTransformation()
 fs = float(1000)/milliseconds_per_instance
 
@@ -74,22 +74,24 @@ DataViz.plot_dataset(data_table, ['acc_phone_x'+'_max_freq', 'acc_phone_x'+'_fre
                                   'like', 'like', 'like', 'like', 'like'], ['line', 'line', 'line', 'line', 'line', 'points'])
 
 
-#print all features
-# for c in periodic_predictor_cols:
-#     data_table = FreqAbs.abstract_frequency(copy.deepcopy(dataset), [c],
-#                                             int(float(10000) / milliseconds_per_instance), fs)
-#     DataViz.plot_dataset(data_table, [c+'_max_freq', c+'_freq_weighted', c+'_pse', c+'_freq_skewness', c+'_freq_kurtosis', 'label'],
-#                          ['like', 'like', 'like', 'like', 'like', 'like'], ['line', 'line', 'line', 'line', 'line', 'points'])
-#dataset = FreqAbs.abstract_frequency(dataset, periodic_predictor_cols, int(float(10000)/milliseconds_per_instance), fs)
+# Spectral analysis of ALL features (generates 18 plots)
+for c in periodic_predictor_cols:
+    data_table = FreqAbs.abstract_frequency(copy.deepcopy(dataset), [c],
+                                            int(float(10000) / milliseconds_per_instance), fs)
+    DataViz.plot_dataset(data_table, [c+'_max_freq', c+'_freq_weighted', c+'_pse', c+'_freq_skewness', c+'_freq_kurtosis', 'label'],
+                         ['like', 'like', 'like', 'like', 'like', 'like'], ['line', 'line', 'line', 'line', 'line', 'points'])
+
+# Compute and add frequency domain features to dataset
+dataset = FreqAbs.abstract_frequency(dataset, periodic_predictor_cols, int(float(10000)/milliseconds_per_instance), fs)
 
 # Now we only take a certain percentage of overlap in the windows, otherwise our training examples will be too much alike.
 
 # The percentage of overlap we allow
-# window_overlap = 0.9
-# skip_points = int((1-window_overlap) * ws)
-# dataset = dataset.iloc[::skip_points,:]
-#
-#
-# dataset.to_csv(dataset_path + 'chapter4_result.csv')
-#
-# DataViz.plot_dataset(dataset, ['acc_phone_x', 'gyr_phone_x', 'hr_watch_rate', 'light_phone_lux', 'mag_phone_x', 'press_phone_', 'pca_1', 'label'], ['like', 'like', 'like', 'like', 'like', 'like', 'like','like'], ['line', 'line', 'line', 'line', 'line', 'line', 'line', 'points'])
+window_overlap = 0.9
+skip_points = int((1-window_overlap) * ws)
+dataset = dataset.iloc[::skip_points,:]
+
+
+dataset.to_csv(dataset_path + 'chapter4_result.csv')
+
+DataViz.plot_dataset(dataset, ['acc_phone_x', 'gyr_phone_x', 'hr_watch_rate', 'light_phone_lux', 'mag_phone_x', 'press_phone_', 'pca_1', 'label'], ['like', 'like', 'like', 'like', 'like', 'like', 'like','like'], ['line', 'line', 'line', 'line', 'line', 'line', 'line', 'points'])
